2ff93b62b92c3a88484c57e354ca63f978d43f8f
chan328 <rkdcks020328@gmail.com>
2018-04-20 20:15:34 +0900

    Add Chan

---
+++ Projrct Negev/Assets/Resources/Chan/scripts/Bulletscript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bulletscript : MonoBehaviour {
+    public float MissileSpeed = 30f;
+    public float DestroyMissileZpos = 18f;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine("DeleteDelay",3);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.Translate(new Vector3(0, 0, 1) * MissileSpeed * Time.deltaTime); 
+	}
+
+    IEnumerator DeleteDelay(int sec)
+    {
+        yield return new WaitForSeconds(sec);
+        Destroy(this.gameObject);
+    }
+}
---
+++ Projrct Negev/Assets/Resources/Chan/scripts/Characterfire.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Characterfire : MonoBehaviour {
+    public GameObject Bullet = null;
+    public float FireTime = 0.5f;
+
+    bool Missile_Fire_State = true;
+	// Use this for initialization
+	void Start () {
+
+	}
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.Z))
+        {
+            if (Missile_Fire_State)
+            {
+                Invoke("FireSpeedController", FireTime);
+                Instantiate(Bullet, transform.position, transform.rotation);
+                Missile_Fire_State = false;
+            }
+        }
+	}
+    void FireSpeedController()
+    {
+        Missile_Fire_State = true;
+    }
+}
