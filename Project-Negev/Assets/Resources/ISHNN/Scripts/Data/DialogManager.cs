3adf7b2ba291b8dfeca65dd6fec25306ef50f883
ISHNN <ishnn032n@gmail.com>
2018-07-13 19:28:08 +0900

    Add DialogSystem

---
+++ Project-Negev/Assets/Resources/ISHNN/Scripts/Data/DialogManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using UnityEngine;
+
+public static class DialogManager {
+    public static List<Dialog> ReadDialog(string dialog_name)
+    {
+        MemoryStream ms = new MemoryStream(Resources.Load<TextAsset>(string.Format("Dialogs/{0}", dialog_name)).bytes);
+        StreamReader sr = new StreamReader(ms, Encoding.Default);
+
+        List<Dialog> dialog = new List<Dialog>();
+
+        while (sr.Peek() != -1)
+        {
+            string D_String = sr.ReadLine();
+            if (D_String == "") continue;
+
+            string[] D_data = D_String.Split('|');
+            dialog.Add(new Dialog(D_data[0], D_data[1]));
+        }
+        return dialog;
+    }
+}
---
+++ Project-Negev/Assets/Resources/ISHNN/Scripts/Menus/DialogUI.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogUI : Menu<DialogUI> {
+    [SerializeField] private Text d_name;
+    [SerializeField] private Text d_script;
+
+    List<Dialog> dialogs = new List<Dialog>();
+
+    int count = 1;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        Time.timeScale = 0;
+    }
+
+    public void SetDIalog(string dialog_name)
+    {
+        dialogs = DialogManager.ReadDialog(dialog_name);
+        d_name.text = dialogs[0].name;
+        d_script.text = dialogs[0].content;
+    }
+    
+    public void OnCliked()
+    {
+        if (count >= dialogs.Count)
+        {
+            Debug.Log("stop");
+            count = 1;
+            Time.timeScale = 1;
+            MenuManager.Instance.CloseMenu();
+        }
+        else
+        {
+            Debug.Log(count);
+            d_name.text = dialogs[count].name;
+            d_script.text = dialogs[count].content;
+            count++;
+        }
+    }
+
+    public override void OnBackPressed()
+    {
+
+    }
+}
